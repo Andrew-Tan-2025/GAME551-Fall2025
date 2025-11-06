13129a503106cb214bdc68ea89634e6c7af96874
Andrew-Tan-622 <95309123+Andrew-Tan-2025@users.noreply.github.com>
2025-11-06 10:03:30 -0700

    wazzap

--- simpleAuth/server.js
+++ simpleAuth/server.js
@@ -2,7 +2,7 @@ var url = require("url"),
 	querystring = require("querystring");
 var passport = require('passport');
 var fs = require('fs');
-	var dbURL = 'mongodb://127.0.0.1:27017/test';
+var dbURL = 'mongodb://127.0.0.1:27017/test';
 var path = require('path'),
   express = require('express'),
   db = require('mongoskin').db(dbURL);
@@ -24,7 +24,7 @@ app.use(session( {store: new MongoStore({
 app.use(passport.initialize());
 app.use(passport.session());
 var flash = require('express-flash');
-app.use( flash() );
+app.use( flash() );      
 
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
@@ -32,7 +32,7 @@ var methodOverride = require("method-override");
 app.use(methodOverride());
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended:false
+  extended:false 
 }));
 require('./passport/config/passport')(passport); // pass passport for configuration
 require('./passport/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
@@ -47,14 +47,7 @@ app.get("/getProjects", function(req,res){
 })
 
 app.use(express.static(path.join(__dirname, 'public')));
-//app.listen(8080);
-
-
-if (require.main === module) { app.listen(8080); }// Instead do export the app:
-else{ module.exports = app; }
-
-
-console.log("server running at http://localhost:8080")
+app.listen(8080);
 
 // route middleware to ensure user is logged in
 function isLoggedIn(req, res, next) {
